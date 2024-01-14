f7b859b4f90fb83cd3c0ffbd380c8e6d04c5cb0f
Philippe Shampanier <pshampanier@gmail.com>
2024-01-14 12:56:02 -0800

    replacing electron by tauri

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   /** need to set the base, otherwise the breakpoint would not bind in vscode */
-  base: "/client/",
+  base: "./",
   build: {
     outDir: "build/webapp",
     sourcemap: true,
@@ -32,4 +32,17 @@ export default defineConfig({
       },
     }),
   ],
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  //
+  // 1. prevent vite from obscuring rust errors
+  clearScreen: false,
+  // 2. tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 1420,
+    strictPort: true,
+    watch: {
+      // 3. tell vite to ignore watching `src-tauri`
+      ignored: ["**/src-tauri/**"],
+    },
+  },
 });
